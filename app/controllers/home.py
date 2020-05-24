e9e95f4e5a9468f0f7179a78d9ddfba5caff6624
Zelin Zhou <zzl@ZelindeMBP.lan>
2020-05-24 11:36:01 -0400

    add a item works with MongoDB now

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,35 @@
+  
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
+from flask import request, redirect
+# from app import db
+import pymongo
+from pymongo import MongoClient
 
 blueprint = Blueprint('home', __name__)
 
+client = pymongo.MongoClient("mongodb://penguin022:Ji0G552V7QT83xCr@cluster0-shard-00-00-yinp0.azure.mongodb.net:27017,cluster0-shard-00-01-yinp0.azure.mongodb.net:27017,cluster0-shard-00-02-yinp0.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.test
+
 @blueprint.route('/')
 def index():
+
+
+    #here we can save the items to an array from the database and then loop through in the index2 template
+
+
     return render_template('home/index2.html')
+
+@blueprint.route('/', methods = ['POST'])
+def addItem():
+    item = {
+        "itemName" : request.form['name'],
+        "description" : request.form['description'],
+        "imageLink" : request.form['image_link']
+    }
+    
+    items= db.items
+    items.insert_one(item)
+
+    
+    return render_template('/home/index2.html')
